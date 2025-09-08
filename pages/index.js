3f013a9c51c3b5eff16fac20bf5f6f7b2eeb1d43
schoenwetteradam <103909524+schoenwetteradam@users.noreply.github.com>
2025-09-07 19:57:12 -0500

    feat: add qr code generator page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import ToolChangeForm from '../components/ToolChangeForm'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="max-w-3xl mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Tool Change Tracker</h1>
       <ToolChangeForm />
+      <div className="mt-4">
+        <Link href="/qr-generator" className="text-blue-600 underline">
+          Generate equipment QR codes
+        </Link>
+      </div>
     </main>
   )
 }
